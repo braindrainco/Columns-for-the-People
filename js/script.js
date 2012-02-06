50beb999b71a0b5a86d658ee6dc557038ba38aaa
Matthew Strom <matthew.h.strom@gmail.com>
2012-02-06 00:26:18 -0600

    Updated to v2, added progress bar, small header, and scrolling behavior.

--- js/script.js
+++ js/script.js
@@ -10,32 +10,72 @@ $(function() {
 });
 
 function setHeight() {
-	var top = $('.copy').offset().top;
+	var top = $('header').height();
 	var windowheight = $(window).height();
-	var diff = windowheight - top - 10;
-	$('.copy').css('height', diff);	
+	var diff = windowheight - top - 40;
+	$('.copy').css({'height':diff, 'margin-top':top});	
 }
 
 function fadeTitle() {
 	var left = $('body').scrollLeft();
 	if ( left !== 0 ){
-		$('header').css('opacity','.4');
+		$('header').addClass('small');
+		//$('section').css('top','5em');
+		setHeight();
 	}
 	else {
-		$('header').css('opacity','1');
+		$('header').removeClass('small');
+		//$('section').css('top','266px');
+		setHeight();
 	}
+}
+
+function progressBar(){
+	var totalWidth = $('.endmark').offset().left - $(window).width();
+	var currentPosition = $('body').scrollLeft();
+	var progress = currentPosition / totalWidth;
+	var progressPercent = progress * 99;
+	var location = Math.round(progressPercent).toFixed(0);
+	var progressLocation = $('#scrollbar').width() * progress;
+	$('#progress').css('width', progressPercent + '%');
+	$('#location').html(location + '%').css('margin-left', progressLocation);
+
+}
+
+function nextParagraph() {
+	var para_currentPosition = $('body').scrollLeft();
+	var paraWidth = $('#first_paragraph').width() + 42;
+    var nextPara = Math.ceil(para_currentPosition / paraWidth) * paraWidth;
+    if (nextPara == para_currentPosition) {
+    nextPara = nextPara + paraWidth;
+    }
+    else{
+    // Do Nothing
+    }
+    return nextPara;
+}
+
+
+function scrolling() {
+	$(document).bind('keydown', 'space', function(){
+		$('body').animate({scrollLeft : nextParagraph()}, 200);
+	});
 }
 
 $(window).ready(function() {
 	setHeight();
 	fadeTitle();
+	progressBar();
+	scrolling();
 });
 
 $(window).resize(function(){
 	setHeight();
+	progressBar();
 });
 
 $(window).scroll(function(){
 	fadeTitle();
+	progressBar();
 });
 
